fc5a8b8759703fac17083db73d2dc84cc1a8382f
AlexanderChiuluvB <16307110258@fudan.edu.cn>
2019-09-29 14:49:01 +0000

    delete .idea

--- miaosha/src/main/java/kafkaConsumer.java
+++ miaosha/src/main/java/kafkaConsumer.java
@@ -1,8 +1,15 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.junit.Test;
+
 public class kafkaConsumer {
 
     public static void main() {
 
+
+
     }
-
-
 }
