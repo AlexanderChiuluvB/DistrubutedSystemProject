107678f9ca14a23aad3b898909210a2703b9e01b
AlexanderChiuluvB <16307110258@fudan.edu.cn>
2019-09-29 15:16:32 +0000

    update kafka

---
+++ miaosha/src/main/java/kafka/kafkaUtil.java
@@ -0,0 +1,31 @@
+package kafka;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+import java.util.Properties;
+
+public class kafkaUtil {
+
+    private static JsonObject getKafkaConfig(String name) throws FileNotFoundException {
+        JsonParser parser = new JsonParser();
+        JsonElement parse = parser.parse(new FileReader("kafka-properties.json"));
+        JsonObject jsonObject = parse.getAsJsonObject().getAsJsonObject(name);
+        System.out.println(jsonObject);
+        return jsonObject;
+    }
+
+    public static Properties getProperties(String name) throws FileNotFoundException {
+        JsonObject config = kafkaUtil.getKafkaConfig(name);
+        Properties properties = new Properties();
+        for (Map.Entry<String, JsonElement> entry : config.entrySet()) {
+            properties.put(entry.getKey(), entry.getValue().getAsString());
+        }
+        return properties;
+    }
+
+}
