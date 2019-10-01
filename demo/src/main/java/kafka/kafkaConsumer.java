2b15b8c684f59fa0adc88fbf5f58a065dd489cf8
AlexanderChiuluvB <16307110258@fudan.edu.cn>
2019-10-01 07:55:53 +0000

    finish kafkaConsumer

--- demo/src/main/java/kafka/kafkaConsumer.java
+++ demo/src/main/java/kafka/kafkaConsumer.java
@@ -1,20 +1,52 @@
 package kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
-import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class kafkaConsumer {
 
-    public void init() {
+    private final static int minBatchSize = 20;
 
+    public static void commit(List<String> topics) throws FileNotFoundException {
+
+        Properties properties = kafkaUtil.getProperties("consumer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(topics);
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records) {
+                buffer.add(record);
+            }
+            if (buffer.size() > minBatchSize) {
+                //do something
+                printBuffer(buffer);
+                consumer.commitAsync(new OffsetCommitCallback() {
+                    @Override
+                    public void onComplete(Map<TopicPartition, OffsetAndMetadata> map, Exception e) {
+                        if (e != null) {
+                            e.printStackTrace();
+                        } else {
+                            for (Map.Entry<TopicPartition, OffsetAndMetadata> entry : map.entrySet()) {
+                                System.out.println(entry.getKey());
+                                System.out.println(entry.getValue());
+                            }
+                        }
+                    }
+                });
+                buffer.clear();
+            }
+        }
     }
 
-    public void commit() {
-
+    private static void printBuffer(List<ConsumerRecord<String, String>> buffer) {
+        buffer.stream().map(ConsumerRecord::value).forEach(System.out::println);
     }
 
 
---
+++ demo/src/test/java/kafka/kafkaConsumerTest.java
@@ -0,0 +1,18 @@
+package kafka;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+
+public class kafkaConsumerTest {
+
+    @Test
+    public void consumerTest() throws FileNotFoundException {
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("mykafka");
+        kafkaConsumer.commit(topics);
+    }
+
+}
